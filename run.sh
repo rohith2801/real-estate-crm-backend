9bae81eb638079128dd185745bf12d37c88b3f5a
Rohith Kumar K <tihor@TIHORs-MacBook-Pro.local>
2024-10-02 15:39:32 +0530

    port change

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ mvn clean package -DskipTests
 IMAGE_NAME="real-estate-crm-backend"
 echo "Building $IMAGE_NAME ......"
 docker build -t "$IMAGE_NAME" .
-docker run -p 9000:8080 "$IMAGE_NAME"
+docker run -p 9228:9228 "$IMAGE_NAME"
